af5a41d348b1ac2f1d9d747d61d1d5577414e6a3
Joshua Wingstrom <josh@eastfive.com>
2019-11-07 14:26:43 -0600

    changed copy style on TableBackups

--- Persistence/StorageTables/Backups/TableBackup.cs
+++ Persistence/StorageTables/Backups/TableBackup.cs
@@ -122,21 +122,73 @@ namespace EastFive.Azure.Persistence.AzureStorageTables.Backups
                             return EastFive.Azure.Persistence.AppSettings.Backup.SecondsGivenToCopyRows.ConfigurationDouble(
                                 async (seconds) =>
                                 {
-                                    var complete = await entity.Copy(
+                                    var invocationMaybe = await entity.Copy(
                                         repoBackup.storageSettingCopyFrom,
                                         repoBackup.storageSettingCopyTo,
-                                        TimeSpan.FromSeconds(seconds),
+                                        100000,
+                                        default,
+                                        requestQuery,
+                                        request,
                                         logger);
-                                    if (complete)
-                                        return onComplete();
+                                    if (invocationMaybe.HasValue)
+                                        return onContinued(invocationMaybe.Value);
 
-                                    var invocationMessage = await requestQuery
-                                        .ById(tableBackupRef)
-                                        .HttpPatch(default)
-                                        .CompileRequest(request)
-                                        .FunctionAsync();
+                                    return onComplete();
 
-                                    return onContinued(invocationMessage);
+                                    //var complete = await entity.Copy(
+                                    //    repoBackup.storageSettingCopyFrom,
+                                    //    repoBackup.storageSettingCopyTo,
+                                    //    TimeSpan.FromSeconds(seconds),
+                                    //    logger);
+                                    //if (complete)
+                                    //    return onComplete();
+
+                                    //var invocationMessage = await requestQuery
+                                    //    .ById(tableBackupRef)
+                                    //    .HttpPatch(default)
+                                    //    .CompileRequest(request)
+                                    //    .FunctionAsync();
+
+                                    //return onContinued(invocationMessage);
+                                },
+                                (why) => onComplete().AsTask());
+                        });
+                },
+                () => onNotFound().AsTask());
+        }
+
+        [HttpPatch]
+        public static async Task<HttpResponseMessage> UpdateAsync(
+                [UpdateId(Name = IdPropertyName)]IRef<TableBackup> tableBackupRef,
+                [Property]string continuationToken,
+                RequestMessage<TableBackup> requestQuery,
+                HttpRequestMessage request,
+                EastFive.Analytics.ILogger logger,
+            CreatedBodyResponse<InvocationMessage> onContinued,
+            NoContentResponse onComplete,
+            NotFoundResponse onNotFound)
+        {
+            return await await tableBackupRef.StorageGetAsync(
+                async entity =>
+                {
+                    return await await entity.backup.StorageGetAsync(
+                        repoBackup =>
+                        {
+                            return AppSettings.Backup.SecondsGivenToCopyRows.ConfigurationDouble(
+                                async (seconds) =>
+                                {
+                                    var invocationMaybe = await entity.Copy(
+                                        repoBackup.storageSettingCopyFrom,
+                                        repoBackup.storageSettingCopyTo,
+                                        100000,
+                                        continuationToken,
+                                        requestQuery,
+                                        request,
+                                        logger);
+                                    if (invocationMaybe.HasValue)
+                                        return onContinued(invocationMaybe.Value);
+
+                                    return onComplete();
                                 },
                                 (why) => onComplete().AsTask());
                         });
@@ -147,6 +199,165 @@ namespace EastFive.Azure.Persistence.AzureStorageTables.Backups
         #endregion
 
         #region Copy Functions
+
+        public async Task<InvocationMessage?> Copy(
+            string storageSettingCopyFrom,
+            string storageSettingCopyTo,
+            int limit,
+            string continuationToken,
+            RequestMessage<TableBackup> requestQuery,
+            HttpRequestMessage request,
+            ILogger logger)
+        {
+            var cloudStorageFromAccount = CloudStorageAccount.Parse(storageSettingCopyFrom);
+            var cloudStorageToAccount = CloudStorageAccount.Parse(storageSettingCopyTo);
+            var cloudStorageFromClient = cloudStorageFromAccount.CreateCloudTableClient();
+            var cloudStorageToClient = cloudStorageToAccount.CreateCloudTableClient();
+
+            var tableFrom = cloudStorageFromClient.GetTableReference(tableName);
+            var tableTo = cloudStorageToClient.GetTableReference(tableName);
+            var query = new TableQuery<GenericTableEntity>();
+
+            var token = default(TableContinuationToken);
+            if (continuationToken.HasBlackSpace())
+            {
+                token = new TableContinuationToken();
+                var tokenReader = XmlReader.Create(new StringReader(continuationToken));
+                token.ReadXml(tokenReader);
+            }
+
+            var segmentFetching = tableFrom.ExecuteQuerySegmentedAsync(query, token);
+            var backoff = TimeSpan.FromSeconds(1.0);
+
+            var completeMutex = new System.Threading.ManualResetEvent(false);
+            var listLock = new object();
+            var rowList = new List<GenericTableEntity>();
+
+            var readingData = ReadData(this);
+            var writingData = Task.Run(() =>  WriteData());
+            continuationToken = await readingData;
+            var invoMsg = continuationToken.HasBlackSpace() ?
+                await requestQuery
+                    .ById(tableBackupRef)
+                    .HttpPatch(
+                        new TableBackup
+                        {
+                            continuationToken = continuationToken,
+                        })
+                    .CompileRequest(request)
+                    .FunctionAsync()
+                :
+                default(InvocationMessage?);
+            completeMutex.Set();
+            await writingData;
+            return invoMsg;
+
+            async Task<string> ReadData(TableBackup tableBackup)
+            {
+                var readCount = 0;
+                while (true)
+                {
+                    try
+                    {
+                        if (segmentFetching.IsDefaultOrNull())
+                            return default;
+
+                        var segment = await segmentFetching;
+                        lock (listLock)
+                        {
+                            rowList.AddRange(segment.Results);
+                            readCount += segment.Results.Count;
+                        }
+
+                        token = segment.ContinuationToken;
+                        if (readCount > limit)
+                        {
+                            if (token.IsDefaultOrNull())
+                                return default;
+
+                            logger.Trace($"Table will be continued: {readCount} partial records [{tableBackup.tableName}]");
+                            using (var writer = new StringWriter())
+                            {
+                                using (var xmlWriter = XmlWriter.Create(writer))
+                                {
+                                    token.WriteXml(xmlWriter);
+                                }
+                                return writer.ToString();
+                            }
+
+                        }
+
+                        segmentFetching = token.IsDefaultOrNull() ?
+                            default
+                            :
+                            tableFrom.ExecuteQuerySegmentedAsync(query, token);
+                    }
+                    catch (StorageException storageEx)
+                    {
+                        if (storageEx.IsProblemTimeout())
+                        {
+                            backoff = backoff + TimeSpan.FromSeconds(1.0);
+                            logger.Trace($"Adjusted backoff to {backoff.TotalSeconds} seconds and pausing");
+                            await Task.Delay(backoff);
+                            segmentFetching = token.IsDefaultOrNull() ?
+                                default
+                                :
+                                tableFrom.ExecuteQuerySegmentedAsync(query, token);
+                            continue;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Warning(JsonConvert.SerializeObject(e));
+                        throw;
+                    };
+                }
+            }
+
+            async Task WriteData()
+            {
+                // TODO: Interleave writes to improve performance
+                while (true)
+                {
+                    GenericTableEntity[] entities;
+                    bool finished = completeMutex.WaitOne(TimeSpan.FromSeconds(0.1));
+                    lock (listLock)
+                    {
+                        entities = rowList.ToArray();
+                        rowList.Clear();
+                    }
+                    if(entities.IsDefaultNullOrEmpty())
+                    {
+                        if (finished)
+                            return;
+                        continue;
+                    }
+                    try
+                    {
+                        TableResult[] resultsProcessingNext = await CreateOrReplaceBatch(entities, tableTo);
+                    }
+                    catch (StorageException storageEx)
+                    {
+                        if (storageEx.IsProblemTimeout())
+                        {
+                            backoff = backoff + TimeSpan.FromSeconds(1.0);
+                            logger.Trace($"Adjusted backoff to {backoff.TotalSeconds} seconds and pausing");
+                            await Task.Delay(backoff);
+                            segmentFetching = token.IsDefaultOrNull() ?
+                                default
+                                :
+                                tableFrom.ExecuteQuerySegmentedAsync(query, token);
+                            continue;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Warning(JsonConvert.SerializeObject(e));
+                        throw;
+                    };
+                }
+            }
+        }
 
         public async Task<bool> Copy(
             string storageSettingCopyFrom,
