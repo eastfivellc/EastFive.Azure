0ddf0ff5d5ebbe4f0c0a317bd95e7689147eb3c0
Joshua Wingstrom <josh@eastfive.com>
2020-07-14 05:34:25 -0500

    added ability to have api tokens

---
+++ Auth/ApiKeyAccessAttribute.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using EastFive.Azure.Auth.CredentialProviders.Voucher;
+using EastFive.Azure.Persistence.AzureStorageTables;
+using EastFive.Extensions;
+using EastFive.Serialization;
+using EastFive.Web.Configuration;
+
+namespace EastFive.Api.Auth
+{
+    public class ApiKeyAccessAttribute : Attribute, IHandleRoutes
+    {
+        public Task<HttpResponseMessage> HandleRouteAsync(Type controllerType, 
+            IApplication httpApp, HttpRequestMessage request, string routeName,
+            RouteHandlingDelegate continueExecution)
+        {
+            if (!request.RequestUri.TryGetQueryParam("api-voucher", out string apiVoucher))
+                return continueExecution(controllerType, httpApp, request, routeName);
+
+            if (request.Headers.Authorization.Scheme.HasBlackSpace())
+                return continueExecution(controllerType, httpApp, request, routeName);
+
+            return EastFive.Security.VoucherTools.ValidateUrlToken(apiVoucher,
+                async (voucherTokenId) =>
+                {
+                    return await await voucherTokenId.AsRef<VoucherToken>()
+                        .StorageGetAsync(
+                            voucherToken =>
+                            {
+                                return EastFive.Security.AppSettings.TokenScope.ConfigurationUri(
+                                    scope =>
+                                    {
+                                        var tokenExpiration = TimeSpan.FromMinutes(1.0);
+                                        request.RequestUri = request.RequestUri.RemoveQueryParameter("api-voucher");
+                                        var sessionId = apiVoucher.MD5HashGuid();
+                                        var claims = voucherToken.claims;
+                                        return JwtTools.CreateToken(sessionId,
+                                                scope, tokenExpiration, claims,
+                                            (tokenNew) =>
+                                            {
+                                                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(tokenNew);
+                                                return continueExecution(controllerType, httpApp, request, routeName);
+                                            },
+                                            (missingConfig) => continueExecution(controllerType, httpApp, request, routeName),
+                                            (configName, issue) => continueExecution(controllerType, httpApp, request, routeName));
+                                    },
+                                    (why) => continueExecution(controllerType, httpApp, request, routeName));
+                            },
+                            () => request
+                                .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                                .AddReason("Voucher token does not exist.")
+                                .AsTask());
+                },
+                why => request
+                    .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                    .AddReason(why)
+                    .AsTask(),
+                why => request
+                        .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                        .AddReason(why)
+                        .AsTask(),
+                    why => request
+                        .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                        .AddReason(why)
+                        .AsTask(),
+                    why => request
+                        .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                        .AddReason(why)
+                        .AsTask(),
+                    (name, why) => request
+                        .CreateResponse(System.Net.HttpStatusCode.Unauthorized)
+                        .AddReason(why)
+                        .AsTask());
+        }
+    }
+}
---
+++ Auth/CredentialProviders/Voucher/VoucherToken.cs
@@ -0,0 +1,134 @@
+﻿using BlackBarLabs.Persistence.Azure.Attributes;
+using EastFive.Api;
+using EastFive.Api.Auth;
+using EastFive.Azure.Persistence.AzureStorageTables;
+using EastFive.Collections.Generic;
+using EastFive.Extensions;
+using EastFive.Linq;
+using EastFive.Persistence;
+using EastFive.Persistence.Azure.StorageTables;
+using EastFive.Security;
+using EastFive.Serialization;
+using EastFive.Web.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Azure.Auth.CredentialProviders.Voucher
+{
+    [FunctionViewController6(
+        Route = "VoucherToken",
+        Resource = typeof(VoucherToken),
+        ContentType = "x-application/auth-voucher-token",
+        ContentTypeVersion = "0.1")]
+    [StorageTable]
+
+    public class VoucherToken : IReferenceable
+    {
+        #region Properties
+
+        [JsonIgnore]
+        public Guid id => voucherTokenRef.id;
+
+        public const string IdPropertyName = "id";
+        [ApiProperty(PropertyName = IdPropertyName)]
+        [JsonProperty(PropertyName = IdPropertyName)]
+        [RowKey]
+        [StandardParititionKey]
+        public IRef<VoucherToken> voucherTokenRef;
+
+        [LastModified]
+        public DateTime lastModified;
+
+        public const string KeySignaturePropertyName = "key_signature";
+        [Storage(Name = KeySignaturePropertyName)]
+        [ApiProperty(PropertyName = KeySignaturePropertyName)]
+        [JsonProperty(PropertyName = KeySignaturePropertyName)]
+        public string keySignature { get; set; }
+
+        public const string KeyPropertyName = "key";
+        [ApiProperty(PropertyName = KeyPropertyName)]
+        [JsonProperty(PropertyName = KeyPropertyName)]
+        public string key { get; set; }
+
+        public const string AuthIdPropertyName = "auth_id";
+        [Storage(Name = AuthIdPropertyName)]
+        [ApiProperty(PropertyName = AuthIdPropertyName)]
+        [JsonProperty(PropertyName = AuthIdPropertyName)]
+        public Guid authId { get; set; }
+
+        public const string TokenPropertyName = "token";
+        [ApiProperty(PropertyName = TokenPropertyName)]
+        [JsonProperty(PropertyName = TokenPropertyName)]
+        public string token { get; set; }
+
+        public const string ExpirationPropertyName = "expiration";
+        [Storage(Name = ExpirationPropertyName)]
+        [ApiProperty(PropertyName = ExpirationPropertyName)]
+        [JsonProperty(PropertyName = ExpirationPropertyName)]
+        public DateTime expiration { get; set; }
+
+        public const string ClaimsPropertyName = "claims";
+        [Storage(Name = ClaimsPropertyName)]
+        [ApiProperty(PropertyName = ClaimsPropertyName)]
+        [JsonProperty(PropertyName = ClaimsPropertyName)]
+        public Dictionary<string, string> claims { get; set; }
+
+        #endregion
+
+        #region Http Methods
+
+        [Api.HttpPost]
+        public async static Task<HttpResponseMessage> CreateAsync(
+                [Property(Name = IdPropertyName)]IRef<VoucherToken> voucherTokenRef,
+                [PropertyOptional(Name = AuthIdPropertyName)]Guid? authorizationIdMaybe,
+                [Property(Name = KeyPropertyName)] string key,
+                [Property(Name = ExpirationPropertyName)] DateTime expiration,
+                [Resource] VoucherToken voucherToken,
+            CreatedBodyResponse<VoucherToken> onCreated,
+            AlreadyExistsResponse onAlreadyExists,
+            GeneralConflictResponse onFailure)
+        {
+            return await VoucherTools.GenerateUrlToken(voucherTokenRef.id, expiration,
+                token =>
+                {
+                    voucherToken.keySignature = key.MD5HashGuid().ToString("N");
+                    voucherToken.token = token;
+                    voucherToken.key = default; // Don't save or return the key
+
+                    if (authorizationIdMaybe.HasValue)
+                    {
+                        voucherToken.claims = voucherToken.claims
+                            .NullToEmpty()
+                            .Append(Api.AppSettings.ActorIdClaimType.ConfigurationString(
+                                (accountIdClaimType) => accountIdClaimType.PairWithValue(authorizationIdMaybe.Value.ToString("N"))))
+                            .Distinct(kvp => kvp.Key)
+                            .ToDictionary();
+                    }
+                    return voucherToken.StorageCreateAsync(
+                        createdId => onCreated(voucherToken),
+                        () => onAlreadyExists());
+                },
+                (why) => onFailure(why).AsTask(),
+                (why, name) => onFailure($"{why}:{name}").AsTask());
+        }
+
+        [HttpAction("whoami")]
+        [ApiKeyAccess]
+        public static HttpResponseMessage WhoAmI(
+                EastFive.Api.Security security,
+            ContentTypeResponse<Guid> onFound)
+        {
+            return onFound(security.performingAsActorId);
+        }
+
+        #endregion
+
+
+    }
+}
