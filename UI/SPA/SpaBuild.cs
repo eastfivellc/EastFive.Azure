fc8c97a2d0dab4f9fb4e5d0035d11dcfba389ea2
Joshua Wingstrom <josh@eastfive.com>
2021-05-25 12:40:01 -0500

    SPA hander fixes

--- UI/SPA/SpaBuild.cs
+++ UI/SPA/SpaBuild.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EastFive.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EastFive.Azure.Spa
@@ -18,6 +20,28 @@ namespace EastFive.Azure.Spa
         public string contentPath;
         public string defaultFile;
         public string indexFile;
+
+        internal string ResolveRoute(string requestPath)
+        {
+            var route = this;
+            var subPath = requestPath.Substring(route.routePrefix.Length);
+            if (subPath.IsDefaultNullOrEmpty())
+                return route.indexFile;
+            if (subPath == "/")
+                return route.indexFile;
+            return $"{route.contentPath}{subPath}";
+        }
+
+        internal string ResolveLocation(string filePath)
+        {
+            var route = this;
+            var subPath = filePath.Substring(route.contentPath.Length);
+            if (subPath.IsDefaultNullOrEmpty())
+                return filePath.Split('/').Last();
+            if (subPath == "/")
+                return route.indexFile.Split('/').Last();
+            return $"{route.routePrefix.TrimEnd('/')}/{subPath.TrimStart('/')}";
+        }
     }
 
     public struct MimeType
