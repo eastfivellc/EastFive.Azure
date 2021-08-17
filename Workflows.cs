960343b21f35bf97f62bac5fcc4c8a2fc8aebe8f
Joshua Wingstrom <josh@eastfive.com>
2021-08-17 12:41:33 -0500

    added postman collection workflow to auth

---
+++ Workflows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Azure
+{
+    public class Workflows
+    {
+        public class AuthorizationFlow
+        {
+            public const string FlowName = "Authorization Flow";
+            public class Variables
+            {
+                public const string TokenName = "TOKEN";
+                public const string AuthHeaderName = "AuthorizationHeaderName";
+            }
+        }
+    }
+}
