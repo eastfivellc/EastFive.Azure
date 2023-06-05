13c3c0decc5900c96e78c1ac563cacb8eb6231b2
Joshua Wingstrom <josh@eastfive.com>
2023-06-05 12:34:00 -0500

    deleted zomebie code and added storage copy props extension

---
+++ Persistence/Extensions/StorageExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EastFive.Extensions;
+using EastFive.Persistence;
+
+namespace EastFive.Azure.Persistence
+{
+	public static class StorageExtensions
+	{
+        public static T CopyStoragePropertiesTo<T>(this T obj, T objectToUpdate)
+        {
+            objectToUpdate = obj
+                .CloneObjectPropertiesWithAttributeInterface(
+                    objectToUpdate, typeof(IPersistInAzureStorageTables), inherit:true);
+            return objectToUpdate;
+        }
+	}
+}
+
