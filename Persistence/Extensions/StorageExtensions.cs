8440bb7b05531e6bfcb1eb56fa0f33248977f8ae
Joshua Wingstrom <josh@eastfive.com>
2023-06-09 09:23:07 -0500

    fixed bug with storage deep copy

--- Persistence/Extensions/StorageExtensions.cs
+++ Persistence/Extensions/StorageExtensions.cs
@@ -6,11 +6,12 @@ namespace EastFive.Azure.Persistence
 {
 	public static class StorageExtensions
 	{
-        public static T CopyStoragePropertiesTo<T>(this T obj, T objectToUpdate)
+        public static T CopyStoragePropertiesTo<T>(this T obj, T objectToUpdate, bool skipNullAndDefault = false)
         {
             objectToUpdate = obj
                 .CloneObjectPropertiesWithAttributeInterface(
-                    objectToUpdate, typeof(IPersistInAzureStorageTables), inherit:true);
+                    objectToUpdate, typeof(IPersistInAzureStorageTables),
+                    skipNullAndDefault, inherit:true);
             return objectToUpdate;
         }
 	}
