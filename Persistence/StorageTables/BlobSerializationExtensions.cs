808d563c516457b77b102a7041de9026155c097d
Joshua Wingstrom <josh@eastfive.com>
2023-06-14 11:00:38 -0500

    new extensions for extracting information from Synapse datalake

---
+++ Persistence/StorageTables/BlobSerializationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using EastFive.Serialization.Parquet;
+
+namespace EastFive.Azure.Persistence.AzureStorageTables
+{
+	public static class BlobSerializationExtensions
+	{
+        public static async Task<TResult> ReadParquetDataFromDataLakeAsync<TResource, TResult>(this string path, string containerName, 
+            Func<TResource[], TResult> onLoaded,
+            Func<TResult> onNotFound)
+		{
+            return await path.BlobLoadBytesAsync(containerName,
+                (payerGapsParquetBytes, properties) =>
+                {
+                    using (var payerGapsParquetStream = new MemoryStream(payerGapsParquetBytes))
+                    {
+                        var claimsLines = payerGapsParquetStream.ParseParquet<TResource>(default, default);
+                        return onLoaded(claimsLines);
+                    }
+                },
+                onNotFound: () =>
+                {
+                    return onNotFound();
+                },
+                connectionStringConfigKey: EastFive.Azure.AppSettings.Persistence.DataLake.ConnectionString);
+        }
+	}
+}
+
