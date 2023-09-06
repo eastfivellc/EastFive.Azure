c9ed698c6ab5700ed1b02bf11f174869ed160854
Joshua Wingstrom <josh@eastfive.com>
2023-09-05 22:20:58 -0500

    added tracking for importing from datalake

---
+++ Diagnostics/AnalyticsLogger.cs
@@ -0,0 +1,49 @@
+﻿
+using EastFive.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace EastFive.Analytics
+{
+    public class AnalyticsLogger : EastFive.Analytics.ILogger
+    {
+        private Microsoft.Extensions.Logging.ILogger logger;
+        private object loggerLock = new object();
+
+        public AnalyticsLogger(Microsoft.Extensions.Logging.ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void LogInformation(string message)
+        {
+            if (logger.IsDefaultOrNull())
+                return;
+            lock (loggerLock)
+                logger.LogInformation(message);
+        }
+
+        public void LogTrace(string message)
+        {
+            if (logger.IsDefaultOrNull())
+                return;
+            lock (loggerLock)
+                logger.LogInformation(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            if (logger.IsDefaultOrNull())
+                return;
+            lock (loggerLock)
+                logger.LogWarning(message);
+        }
+
+        public void LogCritical(string message)
+        {
+            if (logger.IsDefaultOrNull())
+                return;
+            lock (loggerLock)
+                logger.LogCritical(message);
+        }
+    }
+}
--- Functions/FunctionExtensions.cs
+++ Functions/FunctionExtensions.cs
@@ -3,12 +3,18 @@ using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using EastFive.Azure.Persistence.AzureStorageTables;
 using EastFive.Azure.Persistence.Blobs;
 using EastFive.Extensions;
+using EastFive.Analytics;
 using EastFive.Linq.Async;
+
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using System.IO;
+using EastFive.Serialization.Json;
 
 namespace EastFive.Azure.Functions
 {
@@ -17,7 +23,7 @@ namespace EastFive.Azure.Functions
 		public static IEnumerableAsync<(T, string)> SelectWhereNotAlreadyRunning<T>(this IEnumerableAsync<T> resources,
             IDurableOrchestrationClient orchestrationClient,
             Func<T, string> getInstanceId,
-            ILogger log = default,
+            Microsoft.Extensions.Logging.ILogger log = default,
             int? readAhead = default(int?))
         {
             var tpls = resources
@@ -61,15 +67,17 @@ namespace EastFive.Azure.Functions
         public static async Task<(bool, int)> DataLakeIngestAsync<TResource>(
             this IDurableActivityContext context,
             Func<TResource, (string path, string container), int, Task<bool>> processAsync,
-            ILogger log)
+            Microsoft.Extensions.Logging.ILogger log)
         {
             try
             {
+                var instanceId = context.InstanceId.Replace(':', '_');
                 var (path, skip, containerName) = context.GetInput<(string, int, string)>();
                 var startTime = System.Diagnostics.Stopwatch.StartNew();
                 var timeout = TimeSpan.FromMinutes(4);
                 return await DataLakeIngestFileAsync(
                     (path: path, containerName: containerName, skip:skip),
+                    instanceId:instanceId,
                     processAsync: (TResource res, int index) => processAsync(res, (path, containerName), index),
                     isTimedOut: () =>
                     {
@@ -91,64 +99,137 @@ namespace EastFive.Azure.Functions
             }
         }
 
+        public struct Report
+        {
+            public string path;
+            public int linesProcessed;
+            public bool isComplete;
+            public int countProcessed;
+            public Error[] errorsProcessed;
+
+            public struct Error
+            {
+                public int index;
+                public string message;
+            }
+        }
+
         public async static Task<(bool, int)> DataLakeIngestFileAsync<TResource>(
             this (string path, string containerName, int skip) tpl,
+            string instanceId,
             Func<TResource, int, Task<bool>> processAsync,
             Func<bool> isTimedOut,
-            ILogger log)
+            Microsoft.Extensions.Logging.ILogger log)
         {
+            var capturedLog = new EastFive.Analytics.CaptureLog(
+                new Analytics.AnalyticsLogger(log));
             var (path, containerName, skip) = tpl;
+            var dateTime = DateTime.UtcNow;
+            var logPath = path + $".instance-{instanceId}-{dateTime.Year}.{dateTime.Month}.{dateTime.Day}-{dateTime.Hour}_{dateTime.Minute}_{dateTime.Second}.json";
             try
             {
                 return await await path.ReadParquetDataFromDataLakeAsync(containerName,
                     async (TResource[] resourceLines) =>
                     {
-                        if(log.IsNotDefaultOrNull())
-                            log.LogInformation($"[{path}] Loaded--{resourceLines.Length} lines (skipping {skip})");
+                        capturedLog.Information($"[{path}] Loaded--{resourceLines.Length} lines (skipping {skip})");
 
-                        var (linesProcessed, isComplete, countProcessed, errorsProcessed) = await resourceLines
+                        var report = await resourceLines
                             .Skip(skip)
                             .Aggregate(
-                                (index: skip, complete: true, count: 0, errorCount: 0).AsTask(),
-                                async (indexTask, resource) =>
+                                new Report
                                 {
-                                    var (index, complete, count, errorCount) = await indexTask;
+                                    path = path,
+                                    linesProcessed = skip,
+                                    isComplete = true,
+                                    countProcessed = 0,
+                                    errorsProcessed = new Report.Error[] { },
+                                }.AsTask(),
+                                async (reportTask, resource) =>
+                                {
+                                    var currentReport = await reportTask;
+                                    var index = currentReport.linesProcessed;
+                                    var complete = currentReport.isComplete;
+                                    var count = currentReport.countProcessed;
 
                                     var isTimedOutResult = isTimedOut();
                                     if (isTimedOutResult)
-                                        return (index, false, count, errorCount);
+                                    {
+                                        currentReport.isComplete = false;
+                                        return currentReport;
+                                    }
 
                                     try
                                     {
                                         var shouldCount = await processAsync(resource, index);
-                                        return (index + 1, true, count + 1, errorCount);
+                                        currentReport.linesProcessed = currentReport.linesProcessed + 1;
+                                        currentReport.countProcessed = currentReport.countProcessed + 1;
+                                        return currentReport;
                                     }
                                     catch (Exception ex)
                                     {
-                                        if (log.IsNotDefaultOrNull())
-                                            log.LogError($"EXCEPTION [{path}] Line {index}:{ex.Message}");
-                                        return (index + 1, true, count, errorCount + 1);
+                                        capturedLog.Critical($"EXCEPTION [{path}] Line {index}:{ex.Message}");
+                                        currentReport.errorsProcessed = currentReport.errorsProcessed
+                                            .Append(
+                                                new Report.Error
+                                                {
+                                                    index = index,
+                                                    message = ex.Message,
+                                                })
+                                            .ToArray();
+                                        return currentReport;
                                     }
                                 });
 
-                        var state = isComplete ? "completed" : "terminated";
+                        var state = report.isComplete ? "completed" : "terminated";
 
-                        if (log.IsNotDefaultOrNull())
-                            log.LogInformation($"[{path}] {state} at index {linesProcessed} after processing {countProcessed} and {errorsProcessed} errors.");
-                        return (isComplete, linesProcessed);
+                        capturedLog.Information($"[{path}] {state} at index {report.linesProcessed} after processing {report.countProcessed} and {report.errorsProcessed.Length} errors.");
+
+                        try
+                        {
+                            bool success = await logPath.BlobCreateOrUpdateAsync(containerName,
+                                        async stream =>
+                                        {
+                                            var json = report.JsonSerialize(
+                                                j => j);
+                                            var writer = new StreamWriter(stream);
+                                            await writer.WriteAsync(json);
+                                            await writer.FlushAsync();
+                                        },
+                                    onSuccess: contentInfo =>
+                                    {
+                                        return true;
+                                    },
+                                    onFailure: (errorinfo, why) =>
+                                    {
+                                        if (log.IsNotDefaultOrNull())
+                                            log.LogError(why);
+                                        return false;
+                                    },
+                                    contentTypeString: "text/log", fileName: $"{instanceId}.{DateTime.UtcNow.Ticks}.log",
+                                    connectionStringConfigKey:EastFive.Azure.AppSettings.Persistence.DataLake.ConnectionString);
+                        }
+                        catch (Exception ex)
+                        {
+                            if (log.IsNotDefaultOrNull())
+                                log.LogError(ex.Message);
+                        }    
+
+                        return (report.isComplete, report.linesProcessed);
                     },
                     onNotFound: () =>
                     {
-                        if (log.IsNotDefaultOrNull())
-                            log.LogInformation($"[{path}]: Not found");
+                        capturedLog.Information($"[{path}]: Not found");
                         return (true, skip).AsTask();
                     });
             }
             catch (Exception ex)
             {
-                if (log.IsNotDefaultOrNull())
-                    log.LogInformation($"[{path}]:Failure--{ex.Message}");
+                capturedLog.Information($"[{path}]:Failure--{ex.Message}");
                 return (true, 0);
+            }
+            finally
+            {
+                
             }
         }
     }
