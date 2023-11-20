473c79cce69d52026150be966534f6bb46852080
Joshua Wingstrom <josh@eastfive.com>
2023-11-20 08:23:01 -0600

    new search query capabilities in progress

---
+++ Search/Api/SearchQueryApiExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using Azure.Search.Documents.Models;
+
+using EastFive.Extensions;
+using EastFive.Linq.Async;
+using EastFive.Linq;
+using EastFive.Reflection;
+using EastFive.Collections;
+using EastFive.Collections.Generic;
+using EastFive.Web.Configuration;
+using EastFive.Persistence.Azure.StorageTables;
+using EastFive.Azure.Persistence.AzureStorageTables;
+using EastFive.Persistence;
+using EastFive.Serialization.Text;
+using EastFive.Api;
+using EastFive.Azure.Search.Api;
+
+namespace EastFive.Azure.Search
+{
+    public static class SearchQueryApiExtensions
+    {
+        public static IHttpResponse SearchQueryResponse<T>(this IEnumerableAsync<(double?, T)> query,
+            SearchResultsResponse<T> onSearchResults,
+            Dictionary<string, Func<FacetResult[]>> facets = default, Func<long> getTotals = default)
+        {
+            var results = query
+                .Select(tpl => tpl.Item2);
+            return onSearchResults(results,
+                facets,
+                getTotals);
+        }
+
+    }
+}
