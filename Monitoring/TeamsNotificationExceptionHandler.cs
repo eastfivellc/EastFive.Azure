d4d9b36d6b7ad7072a606234f97f6b50bb503829
Brian Hiatt <brian@eastfive.com>
2026-07-23 21:26:38 -0400

    fix: skip Postman link in Teams card when AccessTokenSecret is not configured
    Instead of throwing when EastFive.Api.AccessTokenSecret is missing, pass an
    onSystemNotConfigured callback so the Postman action is simply omitted from
    the notification card. This prevents the exception handler from crashing and
    masking the original error.

--- Monitoring/TeamsNotificationExceptionHandler.cs
+++ Monitoring/TeamsNotificationExceptionHandler.cs
@@ -348,29 +348,29 @@ namespace EastFive.Azure.Monitoring
                     var offset = EastFive.Api.AppSettings.AccessTokenExpirationInMinutes.ConfigurationDouble(
                         (minutes) => TimeSpan.FromMinutes(minutes));
                     var expiresOn = DateTime.UtcNow + offset;
-                    var postmanLink = new QueryableServer<Api.Azure.Monitoring.MonitoringRequest>(request)
+                    actions = new QueryableServer<Api.Azure.Monitoring.MonitoringRequest>(request)
                         .Where(mr => mr.monitoringRequestRef == monitoringRequest.monitoringRequestRef)
                         .Where(mr => mr.when == monitoringRequest.when.Date)
                         .HttpAction(MonitoringRequest.PostmanAction)
                         .Location()
                         .SignWithAccessTokenAccount(Guid.NewGuid(), Guid.NewGuid(), expiresOn,
-                            url => url);
-                    actions = actions
-                        .Append(
-                            new MessageCard.ActionCard
-                            {
-                                name = "Add to Postman Collection",
-                                type = "OpenUri",
-                                targets = new MessageCard.ActionCard.Target[]
-                                {
-                                    new MessageCard.ActionCard.Target
+                            postmanLink => actions
+                                .Append(
+                                    new MessageCard.ActionCard
                                     {
-                                        os = "default",
-                                        uri = postmanLink,
-                                    }
-                                }
-                            })
-                        .ToArray();
+                                        name = "Add to Postman Collection",
+                                        type = "OpenUri",
+                                        targets = new MessageCard.ActionCard.Target[]
+                                        {
+                                            new MessageCard.ActionCard.Target
+                                            {
+                                                os = "default",
+                                                uri = postmanLink,
+                                            }
+                                        }
+                                    })
+                                .ToArray(),
+                            onSystemNotConfigured: () => actions);
                 }
             }
 
